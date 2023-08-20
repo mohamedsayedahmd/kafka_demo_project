20a25634f88a035310b1f15a82d92a92ebdc81cb
mohamed <mohamedsayedahmd@gmail.com>
2023-08-20 16:19:40 +0300

    Producer Logic

--- src/main/java/com/kafka/demo/kafka/controller/service/Producer.java
+++ src/main/java/com/kafka/demo/kafka/controller/service/Producer.java
@@ -1,4 +1,16 @@
 package com.kafka.demo.kafka.controller.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Producer {
+    // we what to send something to kafka ((KafkaTemplate))
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+    public void sendMessageToTopic(String message){
+        kafkaTemplate.send("kafkaDemo_Topic",message);
+    }
 }
+// KafkaTemplate <nameOfTheTopic,message>
