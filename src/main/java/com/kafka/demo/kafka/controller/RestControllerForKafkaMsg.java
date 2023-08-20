5d77c097ebf59d9423e85c515bc599e6bd90a737
mohamed <mohamedsayedahmd@gmail.com>
2023-08-20 16:24:28 +0300

    from the controller to the service

--- src/main/java/com/kafka/demo/kafka/controller/RestControllerForKafkaMsg.java
+++ src/main/java/com/kafka/demo/kafka/controller/RestControllerForKafkaMsg.java
@@ -1,5 +1,7 @@
 package com.kafka.demo.kafka.controller;
 
+import com.kafka.demo.kafka.controller.service.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest/api/")
 public class RestControllerForKafkaMsg {
+
+    @Autowired
+    Producer producer;
+
     @GetMapping("/producerMsg")
     public void getMessageFromClient(@RequestParam("message") String message){
-
+        producer.sendMessageToTopic(message);
     }// message for the producer to say to the topic
 
 }
